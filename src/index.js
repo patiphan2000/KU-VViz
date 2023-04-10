4d6f0130d2a60a73ae294005fc270858c2a6c951
patiphan2000 <patiphan.sri@ku.th>
2023-04-10 09:49:18 +0700

    remove comment

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from 'react'
 import { KuVViz } from './components/index';
 import { courseVerification } from './lib/KUCourseVerification'
 
-// export const ExampleComponent = ({ text }) => {
-//   return <div className={styles.test}>Example Component: {text}</div>
-// }
 
 export { KuVViz, courseVerification };
