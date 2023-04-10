eb23e5c3a0f867cc281580652519d4b2c50101cc
patiphan2000 <patiphan.sri@ku.th>
2023-04-10 09:25:14 +0700

    changed export function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { KuVViz } from './components/index';
+import { courseVerification } from './lib/KUCourseVerification'
 
 // export const ExampleComponent = ({ text }) => {
 //   return <div className={styles.test}>Example Component: {text}</div>
 // }
 
-export { KuVViz } from './components/index';
-export { courseVerification } from './lib/KUCourseVerification'
+export { KuVViz, courseVerification };
